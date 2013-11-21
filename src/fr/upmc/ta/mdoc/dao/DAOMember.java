5ecdac917aee5dae647f3245a32250511033f7e0
marchal vin <marchal.vin@gmail.com>
2013-11-21 13:34:29 +0100

    daomember

---
+++ src/fr/upmc/ta/mdoc/dao/DAOMember.java
@@ -0,0 +1,18 @@
+package fr.upmc.ta.mdoc.dao;
+
+public class DAOMember {
+
+	public static DAOMember instance = new DAOMember();
+	
+	public void createMember() {
+		// TODO
+	}
+	
+	public void updateMember() {
+		// TODO
+	}
+	
+	public void deleteMember() {
+		// TODO
+	}
+}
